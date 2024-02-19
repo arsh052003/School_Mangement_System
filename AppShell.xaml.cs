a43da707551961b34c8597e50c0ab76771a1d1df
16476 <16476@LAPTOP-G13MKOHH>
2024-02-19 17:55:56 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace School_Mangement_System
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
